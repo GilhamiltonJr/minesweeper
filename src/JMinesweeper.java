b527729b46f4b79f48a2ccb8ad2af485ce268f34
Patrick Ring <ringpj09@gmail.com>
2025-05-31 10:40:02 -0700

    added stuff

--- src/JMinesweeper.java
+++ src/JMinesweeper.java
@@ -1,34 +1,51 @@
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.AbstractAction;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
-import javax.swing.UIManager;
 
 public class JMinesweeper {
+	JFrame frame;
 	JPanel mainPanel;
 	JMinefield minefield;
 	JLabel mineCountLabel = new JLabel("");
+	Difficulty[] nextDifficulty = new Difficulty[1];
 	
 	public static void main(String args[]) {
 		new JMinesweeper();
 	}
 	
 	public JMinesweeper() {
-		JFrame frame = new JFrame("Minesweeper");
+		frame = new JFrame("Minesweeper");
 		mainPanel = new JPanel(new BorderLayout());
 		JToolBar toolBar = new JToolBar();
 		toolBar.add(new AbstractAction("New Game") {
-			
 			//overrides the default action preformed in the anonymous subclass
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				newGame();
 			}
 		});
+		JComboBox<Difficulty> difficultyBox = new JComboBox<>();
+		difficultyBox.addItem(new Difficulty("Beginner", 9, 9, 10, 70));
+		difficultyBox.addItem(nextDifficulty[0] = new Difficulty("Intermediate", 16, 16, 40, 50));
+		difficultyBox.addItem(new Difficulty("Expert", 30, 16, 99, 40));
+
+		difficultyBox.addItemListener(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent e) {
+				nextDifficulty[0] = (Difficulty) e.getItem();
+				newGame();
+			}
+		});
+		
+		toolBar.add(difficultyBox);
 		toolBar.add(mineCountLabel);
 		mainPanel.add(toolBar, BorderLayout.NORTH);
 		toolBar.setFloatable(false);
@@ -43,15 +60,23 @@ public class JMinesweeper {
 		if(minefield != null) {
 			mainPanel.remove(minefield);
 		}
-		Minefield m = new Minefield(20,20,38);
+		Minefield m = new Minefield(nextDifficulty[0].width(),nextDifficulty[0].height(),nextDifficulty[0].mineCount());
 		m.addGameListener(new GameListener() {
 			@Override
 			public void update() {
 				mineCountLabel.setText("Mines Flagged: " + m.getFlaggedMineCount() + " / " + m.getMineCount());
 			}
 		});
-		minefield = new JMinefield(m, 50);//8,8,10 for easy, 20,20,38 for medium, 30,16,99 for hard
+		minefield = new JMinefield(m, nextDifficulty[0].dimension);//8,8,10 for easy, 20,20,38 for medium, 30,16,99 for hard
 		mainPanel.add(minefield, BorderLayout.CENTER);
 		mainPanel.revalidate();
+		frame.pack();
+	}
+	
+	record Difficulty(String name, int width, int height, int mineCount, int dimension) {
+		@Override
+		public String toString() {
+			return String.format("%s (%dx%d, %d mines)", name, width, height, mineCount);
+		}
 	}
 }
