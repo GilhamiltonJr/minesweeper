8e201da4c07191ee9af0a546062e848014d7b553
James Ring <sjr@jdns.org>
2025-06-05 20:32:32 -0700

    add timer

--- src/main/java/com/github/gilhamiltonjr/minesweeper/JMinesweeper.java
+++ src/main/java/com/github/gilhamiltonjr/minesweeper/JMinesweeper.java
@@ -4,28 +4,34 @@ import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-
 import javax.swing.AbstractAction;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JSeparator;
 import javax.swing.JToolBar;
+import javax.swing.Timer;
 
 public class JMinesweeper {
 	JFrame frame;
 	JPanel mainPanel;
 	JMinefield minefield;
 	JLabel mineCountLabel = new JLabel("");
+	JLabel timeLabel = new JLabel("0:00");
 	final Difficulty[] nextDifficulty = new Difficulty[1];
 	final Gamemode[] gamemode = new Gamemode[1];
-	
-	public static void main(String args[]) {
+	final Timer gameTimer;
+	long gameStartTime = 0;
+
+	public static void main(String[] args) {
 		new JMinesweeper();
 	}
 	
 	public JMinesweeper() {
 		frame = new JFrame("Minesweeper");
+		gameTimer = new Timer(50, this::updateTimerLabel);
+		gameTimer.setRepeats(true);
 		mainPanel = new JPanel(new BorderLayout());
 		JToolBar toolBar = new JToolBar();
 		toolBar.add(new AbstractAction("New Game") {
@@ -46,7 +52,6 @@ public class JMinesweeper {
 				nextDifficulty[0] = (Difficulty) e.getItem();
 				//making it final is necessary so i gotta make it a final array so you can edit the contents still :D
 				newGame();
-				
 			}
 		});
 		
@@ -68,6 +73,8 @@ public class JMinesweeper {
 		toolBar.add(difficultyBox);
 		toolBar.add(gamemodeBox);
 		toolBar.add(mineCountLabel);
+		toolBar.add(new JSeparator());
+		toolBar.add(timeLabel);
 		mainPanel.add(toolBar, BorderLayout.NORTH);
 		toolBar.setFloatable(false);
 		newGame();
@@ -77,6 +84,25 @@ public class JMinesweeper {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 	}
+
+	private void startTimer() {
+		gameStartTime = System.nanoTime();
+		gameTimer.start();
+	}
+
+	private void stopTimer() {
+		gameTimer.stop();
+		gameStartTime = 0;
+	}
+
+	private void updateTimerLabel(ActionEvent e) {
+		if (gameStartTime == 0) {
+			return;
+		}
+		long elapsedNanos = System.nanoTime() - gameStartTime;
+		timeLabel.setText(String.format("%d:%02d", elapsedNanos / (60 * 1000000000L), (elapsedNanos / 1000000000L) % 60));
+	}
+
 	public void newGame() {
 		if(minefield != null) {
 			mainPanel.remove(minefield);
@@ -87,6 +113,11 @@ public class JMinesweeper {
 			@Override
 			public void update() {
 				mineCountLabel.setText("Mines Flagged: " + m.getFlaggedMineCount() + " / " + m.getMineCount());
+				if (m.getIsGameOver() || m.isGameWon()) {
+					stopTimer();
+				} else if (!gameTimer.isRunning()) {
+					startTimer();
+				}
 			}
 		});
 		minefield = new JMinefield(m, nextDifficulty[0].dimension);//8,8,10 for easy, 20,20,38 for medium, 30,16,99 for hard
@@ -94,6 +125,8 @@ public class JMinesweeper {
 		minefield.setReduction(gamemode[0].getReduction());
 		mainPanel.add(minefield, BorderLayout.CENTER);
 		mainPanel.revalidate();
+		timeLabel.setText("0:00");
+		stopTimer();
 		frame.pack();
 	}
 	
